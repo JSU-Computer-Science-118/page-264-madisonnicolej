df5783dada28d4afc56fcdf630142f14b0583b8d
madisonnicolej <117469674+madisonnicolej@users.noreply.github.com>
2022-11-10 18:03:40 -0600

    final commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-//Madison Jones			J00964128
-//Page 264 Exercise 22	11/8/22
-
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -9,22 +6,62 @@ int main()
 {
 	int number, x;
 	bool prime = true;
-	
+
 	cin >> number;
 
-	if (number > 1000 || number <=1) {
+	if (number > 1000 || number <=1 ) {
 		prime = false;
 		cout << "out of bound";
 		return 0;
 	}
-
+	
 	for (x = 2; x <= number / 2; x++)
 	{
-		if (number % x == 0) {	// if remainder of divisor is 0, number is not prime
+		if (number % x == 0 && x==2) {	// if remainder of divisor is 0, number is not prime
 			prime = false;
 			cout << x << " ";
 		}
-		
+		if (number % x == 0 && x==3) {	
+			prime = false;
+			cout << x << " ";
+		}
+		if (number % x == 0 && x==5) {	
+					prime = false;
+					cout << x << " ";
+				}
+		if (number % x == 0 && x==7) {	
+					prime = false;
+					cout << x << " ";
+				}
+		if (number % x == 0 && x==11) {	
+					prime = false;
+					cout << x << " ";
+				}
+		if (number % x == 0 && x==13) {	
+					prime = false;
+					cout << x << " ";
+				}
+		if (number % x == 0 && x==17) {	
+					prime = false;
+					cout << x << " ";
+				}
+		if (number % x == 0 && x==19) {	
+					prime = false;
+					cout << x << " ";
+				}
+if (number % x == 0 && x==23) {	
+					prime = false;
+					cout << x << " ";
+				}
+    if (number % x == 0 && x==29) {	
+					prime = false;
+					cout << x << " ";
+				}
+    if (number % x == 0 && x==31) {	
+					prime = false;
+					cout << x << " ";
+				}
+		//2, 3, 5, 7, 11, 13, 17, 19, 23, 29, and 31
 	}
 	if (prime == true)
 	cout << "Number " << number << " is prime!";
